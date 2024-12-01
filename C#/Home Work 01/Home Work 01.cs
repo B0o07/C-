cd12c0a4a73c9244a4f9ca1e135023039490e2da
Saulo <67021707+B0o07@users.noreply.github.com>
2024-12-01 15:52:04 -0300

    Add files via upload

---
+++ C#/Home Work 01/Home Work 01.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Home_Work_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change the appearance
+
+            Console.Title = "Soldiers";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+
+            Console.WriteLine("A soldier by the name...");
+            Console.ReadLine();
+
+            Console.WriteLine("are having a conversation with other soldier named...");
+            Console.ReadLine();
+
+            Console.WriteLine("Suddenly they hear a noise, then when they get closer  to the noise the see...");
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Then they go away.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/My Awesome Program/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace My_Awesome_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change the appearance
+
+            Console.Title = "Skynet";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+
+            // Get a conversation going
+
+            Console.WriteLine("Hello, what's your name?");
+
+            Console.ReadLine(); // This make possible to user write in the terminal!
+
+            Console.WriteLine("My name is RX-9000. \nI'm an AI sent from the future to destrou mankind ;)");
+            Console.WriteLine("What is your favvoritte color?");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Cool! Mine is destruction.");
+
+            Console.ReadKey();
+        }
+    }
+}
